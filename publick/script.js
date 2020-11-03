b5c107c697893abc2989459c15d88c049bb79850
KudravcevPavel <kudravcev92@gmail.com>
2020-11-03 17:19:57 +0300

    new try burger

---
+++ publick/script.js
@@ -0,0 +1,16 @@
+const headerBurger = document.querySelector(".header__burger");
+const headerMenu = document.querySelector(".header__menu");
+const back = document.querySelector("body");
+const menuList = document.querySelector(".menu__list");
+
+headerBurger.onclick = function () {
+   headerBurger.classList.toggle("active");
+   headerMenu.classList.toggle("active");
+   back.classList.toggle("lock");
+};
+
+menuList.onclick = function () {
+   menuList.classList.remove("active");
+   back.classList.toggle("lock");
+};
+
